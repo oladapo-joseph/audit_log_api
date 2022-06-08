b23e39a2caff5db9442cdf46e29acb2201e6ad9b
Adeleke Oladapo Joseph <oladapo_joseph@yahoo.com>
2022-06-08 16:52:31 +0100

    for online mongodb use

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ cluster = get_key('.env', "CLUSTER")
 
 class DevConfig:
    MONGODB_SETTINGS = { 
-                        "host" : 'mongodb://localhost:27017/test' #for local connection
-                    #  'host' : f"mongodb+srv://{user}:{password}@{cluster}/test?retryWrites=true&ssl=true&tlsAllowInvalidCertificates=true"
+                        # "host" : 'mongodb://localhost:27017/test' #for local connection
+                     'host' : f"mongodb+srv://{user}:{password}@{cluster}/test?retryWrites=true&ssl=true&tlsAllowInvalidCertificates=true"
                         }
     
     
